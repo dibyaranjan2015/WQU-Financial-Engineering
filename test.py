0e42ac00ce8e174300096ac0c7dce1bd16674b31
dibyaranjan2015 <dibyaranjan.avanti@gmail.com>
2026-07-08 21:23:17 +0530

    Initial test

---
+++ test.py
@@ -0,0 +1,7 @@
+# A simple check to see everything is working
+assets = ["AAPL", "MSFT", "GOOG", "NVDA"]
+
+print("Starting my WQU Quantititive Journey!")
+print(f"Tracking a universe of {len(assets)} assets:")
+for asset in assets:
+    print(f" - {asset}")
